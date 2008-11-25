8e361d19aaf046abd52ee21999e9631bb8873271
lode_leroy <lode_leroy@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2008-11-25 08:12:23 +0000

    fix verticals in DropCutter
    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@17 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- pycam/PathGenerators/DropCutter.py
+++ pycam/PathGenerators/DropCutter.py
@@ -3,7 +3,7 @@ from pycam.Geometry import *
 from pycam.Geometry.utils import *
 
 class DropCutter:
-    
+
     def __init__(self, cutter, model, PathProcessor=None):
         self.cutter = cutter
         self.model = model
@@ -21,10 +21,12 @@ class DropCutter:
             while y<=maxy:
                 x = minx
                 pa.new_scanline()
+                t_last = None
                 while x<=maxx:
                     p = Point(x,y,z1)
                     z_max = -INFINITE
                     cl_max = None
+                    t_max = None
                     self.cutter.moveto(p)
                     for t in self.model.triangles():
                         if t.normal().z < 0: continue;
@@ -32,10 +34,36 @@ class DropCutter:
                         if cl and (cl.z > z_max or cl_max is None):
                             z_max = cl.z
                             cl_max = cl
+                            t_max = t
                     if not cl_max or cl_max.z<z0:
                         cl_max = Point(x,y,z0)
+
+                    if (t_max and not t_last) or (t_last and not t_max):
+                        if cl_last.z < z_max:
+                            pa.append(Point(cl_last.x,cl_last.y,cl_max.z))
+                        else:
+                            pa.append(Point(cl_max.x,cl_max.y,cl_last.z))
+                    elif (t_max and t_last and cl_last and cl_max ) and (t_max != t_last):
+                        nxl = t_last.normal().x
+                        nzl = t_last.normal().z
+                        nxm = t_max.normal().x
+                        nzm = t_max.normal().z
+                        xl = cl_last.x
+                        zl = cl_last.z
+                        xm = cl_max.x
+                        zm = cl_max.z
+                        try:
+                            X = (zl-zm+(xm*nxm/nzm+xl*nxl/nzl))/(nxm/nzm+nxl/nzl)
+                            Y = cl_last.y
+                            Z = zl + (X-xl)*nxl/nzm
+                            if xl > X and X < xm:
+                                pa.append(Point(X,Y,Z))
+                        except:
+                            pass
                     pa.append(cl_max)
 
+                    cl_last = cl_max
+                    t_last = t_max
                     x += dx
 
                 pa.end_scanline()
@@ -48,10 +76,12 @@ class DropCutter:
             while x<=maxx:
                 y = miny
                 pa.new_scanline()
+                t_last = None
                 while y<=maxy:
                     p = Point(x,y,z1)
                     z_max = -INFINITE
                     cl_max = None
+                    t_max = None
                     self.cutter.moveto(p)
                     for t in self.model.triangles():
                         if t.normal().z < 0: continue;
@@ -59,10 +89,38 @@ class DropCutter:
                         if cl and (cl.z > z_max or cl_max is None):
                             z_max = cl.z
                             cl_max = cl
+                            t_max = t
                     if not cl_max or cl_max.z<z0:
                         cl_max = Point(x,y,z0)
+
+
+                    if (t_max and not t_last) or (t_last and not t_max):
+                        if cl_last.z < z_max:
+                            pa.append(Point(cl_last.x,cl_last.y,cl_max.z))
+                        else:
+                            pa.append(Point(cl_max.x,cl_max.y,cl_last.z))
+                    elif (t_max and t_last and cl_last and cl_max ) and (t_max != t_last):
+                        nyl = t_last.normal().y
+                        nzl = t_last.normal().z
+                        nym = t_max.normal().y
+                        nzm = t_max.normal().z
+                        yl = cl_last.y
+                        zl = cl_last.z
+                        ym = cl_max.y
+                        zm = cl_max.z
+                        try:
+                            X = cl_last.x
+                            Y = (zl-zm+(ym*nym/nzm+yl*nyl/nzl))/(nym/nzm+nyl/nzl)
+                            Z = zl + (Y-yl)*nyl/nzm
+                            if yl > Y and Y < ym:
+                                pa.append(Point(X,Y,Z))
+                        except:
+                            pass
+
                     pa.append(cl_max)
 
+                    cl_last = cl_max
+                    t_last = t_max
                     y += dy
 
                 pa.end_scanline()
