c3685f3e50cd47efbc4dc732a24c613837da78c0
lode_leroy <lode_leroy@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2008-11-28 08:58:27 +0000

    guard against float division
    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@22 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- pycam/Geometry/intersection.py
+++ pycam/Geometry/intersection.py
@@ -16,25 +16,24 @@ def intersect_lines(xl,zl,nxl,nzl,xm,zm,nxm,nzm):
     X = None
     Z = None
 #    print "xl=",xl,", zl=",zl,"nxl=",nxl,", nzl=",nzl,", X=", X, ", Z=",Z,", xm=",xm,",zm=",zm, ", nxm=",nxm,",nzm=",nzm
-    if isZero(nzl) and isZero(nzm):
-        pass
-    elif isZero(nzl) or isZero(nxl):
-        X = xl
-        Z = zm + (xm-xl)*nxm/nzm
-        return (X,Z)
-    elif isZero(nzm) or isZero(nxm):
-        X = xm
-        Z = zl - (xm-xl)*nxl/nzl
-        return (X,Z)
-    else:
-        try:
-            X = (zl-zm+(xm*nxm/nzm-xl*nxl/nzl))/(nxm/nzm-nxl/nzl)
-        except:
+    try:
+        if isZero(nzl) and isZero(nzm):
             pass
-
-        if X and xl < X and X < xm:
-            Z = zl + (X-xl)*nxl/nzl
+        elif isZero(nzl) or isZero(nxl):
+            X = xl
+            Z = zm + (xm-xl)*nxm/nzm
             return (X,Z)
+        elif isZero(nzm) or isZero(nxm):
+            X = xm
+            Z = zl - (xm-xl)*nxl/nzl
+            return (X,Z)
+        else:
+            X = (zl-zm+(xm*nxm/nzm-xl*nxl/nzl))/(nxm/nzm-nxl/nzl)
+            if X and xl < X and X < xm:
+                Z = zl + (X-xl)*nxl/nzl
+                return (X,Z)
+    except:
+        pass
     return (None,None)
 
 def intersect_plane_point(plane, direction, point):
