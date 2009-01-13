b8027f697d95b21dc1a9157128188c4ffe06ca64
lode_leroy <lode_leroy@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2009-01-13 07:41:56 +0000

    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@29 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- pycam.py
+++ pycam.py
@@ -1,7 +1,5 @@
 import sys
 import os
-sys.path.insert(0, os.path.join(sys.prefix, "PyOpenGL-3.0.0b5-py2.5.egg"))
-sys.path.insert(0, os.path.join(sys.prefix, "setuptools-0.6c8-py2.5.egg"))
 
 from pycam.Gui.SimpleGui import SimpleGui
 from pycam.Importers.TestModel import TestModel
