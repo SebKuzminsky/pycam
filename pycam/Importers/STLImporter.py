8da6f012b5059838dcbb4e7c3542603060436dcf
lode_leroy <lode_leroy@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2010-01-18 11:39:27 +0000

    https://sourceforge.net/tracker/?func=detail&aid=2933162&group_id=237831&atid=1104178
    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@60 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- pycam/Importers/STLImporter.py
+++ pycam/Importers/STLImporter.py
@@ -166,10 +166,18 @@ def ImportModel(filename, use_kdtree=True):
             m = endfacet.match(line)
             if m:
                 # make sure the points are in ClockWise order
-                if n.dot(p3.sub(p1).cross(p2.sub(p1)))>0:
+                dotcross = n.dot(p3.sub(p1).cross(p2.sub(p1)))
+                if dotcross > 0:
                     t = Triangle(p1, p2, p3, UniqueEdge(p1,p2), UniqueEdge(p2,p3), UniqueEdge(p3,p1))
-                else:
+                elif dotcross < 0:
                     t = Triangle(p1, p3, p2, UniqueEdge(p1,p3), UniqueEdge(p3,p2), UniqueEdge(p2,p1))
+                else:
+                    # the three points are in a line - or two points are identical
+                    # usually this is caused by points, that are too close together
+                    # check the tolerance value in pycam/Geometry/PointKdtree.py
+                    print "ERROR: skipping irregular triangle: %s / %s / %s" % (p1, p2, p3)
+                    n=p1=p2=p3=None
+                    continue
                 t._normal = n
                 n=p1=p2=p3=None
                 if t.normal().z < 0:
