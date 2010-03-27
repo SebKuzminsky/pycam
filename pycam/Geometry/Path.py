7793791de8699e6d38d80f9c54492bd7755a064d
sumpfralle <sumpfralle@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2010-03-27 02:39:50 +0000

    reduce memory consumption of stored toolpaths to 1/3 (applies only to python 2.6 or above)
    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@270 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- pycam/Geometry/Path.py
+++ pycam/Geometry/Path.py
@@ -1,3 +1,17 @@
+""" the points of a path are only used for describing coordinates. Thus we
+don't really need complete "Point" instances that consume a lot of memory.
+Since python 2.6 the "namedtuple" factory is available.
+This reduces the memory consumption of a toolpath down to 1/3.
+"""
+try:
+    # this works for python 2.6 or above (saves memory)
+    from collections import namedtuple
+    tuple_point = collections.namedtuple("TuplePoint", "x y z")
+    get_point_object = lambda point: tuple_point(point.x, point.y, point.z)
+except ImportError:
+    # dummy for python < v2.6 (consumes more memory)
+    get_point_object = lambda point: point
+
 
 class Path:
     id = 0
@@ -22,7 +36,7 @@ class Path:
         return s
 
     def append(self, p):
-        self.points.append(p)
+        self.points.append(get_point_object(p))
 
     def reverse(self):
         self.points.reverse()
