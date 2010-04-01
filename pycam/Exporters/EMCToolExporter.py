5a126a161a7e6c239136b28a6e08e18963c6f4a3
sumpfralle <sumpfralle@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2010-04-01 14:26:38 +0000

    EMC uses a diameter instead of the radius
    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@280 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- pycam/Exporters/EMCToolExporter.py
+++ pycam/Exporters/EMCToolExporter.py
@@ -12,7 +12,7 @@ class EMCToolExporter:
             tool = self.tools[index]
             # use an arbitrary length
             tool_length = tool["tool_radius"] * 10
-            line = "T%d P%d D%f Z-%f ;%s" % (index + 1, index + 1, tool["tool_radius"], tool_length, tool["name"])
+            line = "T%d P%d D%f Z-%f ;%s" % (index + 1, index + 1, 2 * tool["tool_radius"], tool_length, tool["name"])
             result.append(line)
         # add the dummy line for the "last" tool
         result.append("T99999 P99999 Z+0.100000 ;dummy tool")
