f1f70c97f1f91f34908c1573dd7813ed56caae05
sumpfralle <sumpfralle@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2012-01-03 18:59:10 +0000

    fixed issue with Python 2.5 (tuples don't offer "index")
    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@1224 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- src/pycam/Plugins/Bounds.py
+++ src/pycam/Plugins/Bounds.py
@@ -412,8 +412,10 @@ class BoundsDict(pycam.Plugins.ObjectWithAttributes):
                 "BoundaryHighZ": 0,
                 "TypeRelativeMargin": True,
                 "TypeCustom": False,
-                "RelativeUnit": _RELATIVE_UNIT.index("%"),
-                "ToolLimit": _BOUNDARY_MODES.index("along"),
+                # Use "list" conversion here: python 2.5 does not support
+                # "index" for tuples.
+                "RelativeUnit": list(_RELATIVE_UNIT).index("%"),
+                "ToolLimit": list(_BOUNDARY_MODES).index("along"),
                 "Models": [],
         })
 
