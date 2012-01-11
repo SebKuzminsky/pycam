d6117525dae4cbad11b9026cde9cd696060b8ea2
sumpfralle <sumpfralle@bbaffbd6-741e-11dd-a85d-61de82d9cad9>
2012-01-11 12:43:51 +0000

    added dependency "rsvg" (for svg icons via gdkpixbufloader)
    git-svn-id: https://pycam.svn.sourceforge.net/svnroot/pycam/trunk@1229 bbaffbd6-741e-11dd-a85d-61de82d9cad9

--- pyinstaller/hooks/hook-pycam.py
+++ pyinstaller/hooks/hook-pycam.py
@@ -1,4 +1,5 @@
 # keysyms does not seem to be recognized by pyinstaller
 # There will be exceptions after any keypress without this line.
-hiddenimports = ["gtk.keysyms", "decimal", "uuid", "ConfigParser", "subprocess", "platform", "webbrowser", "OpenGL.GLUT"]
+# rsvg is required for SVG icons (dynamically loaded on demand)
+hiddenimports = ["gtk.keysyms", "rsvg"]
 
