12a6899c1d0f68f5f23475221c89108bf435601a
Sebastian Kuzminsky <seb@highlab.com>
2017-02-19 17:10:01 -0700

    travis: install build dependencies better

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 sudo apt-get update -qq
 sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
-mk-build-deps -i -r -s sudo -t 'apt-get --no-install-recommends --no-install-suggests'
+mk-build-deps -i -r -s sudo -t 'apt-get --yes --no-install-recommends --no-install-suggests'
