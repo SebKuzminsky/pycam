9e0570a0e54aafe3963050d544960504c091b431
Sebastian Kuzminsky <seb@highlab.com>
2017-03-01 15:52:04 -0700

    fixup pycam/__init__.py: try to get version from git if no Version.py exists

--- pycam/__init__.py
+++ pycam/__init__.py
@@ -18,10 +18,25 @@ You should have received a copy of the GNU General Public License
 along with PyCAM.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
+import os
+import subprocess
+
+
 try:
     from Version import VERSION
 except:
-    VERSION = "0.0-unknown"
+    # Failed to import Version.py, we must be running out of a git
+    # checkout where the user forgot to run `make pycam/Version.py`.
+    repo_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    git_proc = subprocess.Popen(["./scripts/get-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=repo_dir)
+    stdout, stderr = git_proc.communicate()
+    if git_proc.returncode == 0:
+	VERSION = stdout.strip()
+    else:
+	# No pycam/Version.py and git failed to give us a version number, give up.
+	VERSION = "0.0-unknown"
+
 
 FILTER_CONFIG = (("Config files", "*.conf"),)
 HELP_WIKI_URL = "http://sourceforge.net/apps/mediawiki/pycam/index.php?title=%s"
