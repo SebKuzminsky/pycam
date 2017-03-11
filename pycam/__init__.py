0d384923f02054c54cd98c54c3124d73833eca2a
Lars Kruse <devel@sumpfralle.de>
2017-03-11 04:09:42 +0100

    bump version to v0.6.2-dev

--- pycam/__init__.py
+++ pycam/__init__.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with PyCAM.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-VERSION = "0.6.1"
+VERSION = "0.6.2-dev"
 
 FILTER_CONFIG = (("Config files", "*.conf"),)
 DOC_BASE_URL = "http://pycam.sourceforge.net/%s/"
