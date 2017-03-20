66b4a60b32ab71b17fd7f2653464c05bbe9f9264
Lars Kruse <devel@sumpfralle.de>
2017-03-21 00:03:57 +0100

    utils.events: migrate tuples to namedtuples for readability

--- pycam/Utils/events.py
+++ pycam/Utils/events.py
@@ -1,3 +1,5 @@
+import collections
+
 import pycam.Gui.Settings
 import pycam.Utils.log
 
@@ -5,11 +7,11 @@ import pycam.Utils.log
 log = pycam.Utils.log.get_logger()
 
 
-UI_FUNC_INDEX, UI_WIDGET_INDEX = range(2)
-WIDGET_NAME_INDEX, WIDGET_OBJ_INDEX, WIDGET_WEIGHT_INDEX, WIDGET_ARGS_INDEX = range(4)
-HANDLER_FUNC_INDEX, HANDLER_ARG_INDEX = range(2)
-EVENT_HANDLER_INDEX, EVENT_BLOCKER_INDEX = range(2)
-CHAIN_FUNC_INDEX, CHAIN_WEIGHT_INDEX = range(2)
+UISection = collections.namedtuple("UISection", ("add_func", "clear_func", "widgets"))
+UIWidget = collections.namedtuple("UIWidget", ("name", "obj", "weight", "args"))
+UIHandler = collections.namedtuple("UIHandler", ("func", "args"))
+UIEvent = collections.namedtuple("UIEvent", ("handlers", "blocker_tokens"))
+UIChain = collections.namedtuple("UIChain", ("func", "weight"))
 
 
 class EventCore(pycam.Gui.Settings.Settings):
@@ -24,51 +26,45 @@ class EventCore(pycam.Gui.Settings.Settings):
 
     def register_event(self, event, func, *args):
         if event not in self.event_handlers:
-            assert EVENT_HANDLER_INDEX == 0
-            assert EVENT_BLOCKER_INDEX == 1
-            self.event_handlers[event] = [[], 0]
-        assert HANDLER_FUNC_INDEX == 0
-        assert HANDLER_ARG_INDEX == 1
-        self.event_handlers[event][EVENT_HANDLER_INDEX].append((func, args))
+            self.event_handlers[event] = UIEvent([], [])
+        self.event_handlers[event].handlers.append(UIHandler(func, args))
 
     def unregister_event(self, event, func):
         if event in self.event_handlers:
             removal_list = []
             handlers = self.event_handlers[event]
-            for index, item in enumerate(handlers[EVENT_HANDLER_INDEX]):
-                if func == item[HANDLER_FUNC_INDEX]:
+            for index, item in enumerate(handlers.handlers):
+                if func == item.func:
                     removal_list.append(index)
             removal_list.reverse()
             for index in removal_list:
-                handlers[EVENT_HANDLER_INDEX].pop(index)
+                handlers.handlers.pop(index)
         else:
             log.debug("Trying to unregister an unknown event: %s", event)
 
     def emit_event(self, event, *args, **kwargs):
         log.debug2("Event emitted: %s", str(event))
         if event in self.event_handlers:
-            if self.event_handlers[event][EVENT_BLOCKER_INDEX] != 0:
+            if self.event_handlers[event].blocker_tokens:
                 return
             # prevent infinite recursion
             self.block_event(event)
-            for handler in self.event_handlers[event][EVENT_HANDLER_INDEX]:
-                func = handler[HANDLER_FUNC_INDEX]
-                data = handler[HANDLER_ARG_INDEX]
-                func(*(data + args), **kwargs)
+            for handler in self.event_handlers[event].handlers:
+                handler.func(*(handler.args + args), **kwargs)
             self.unblock_event(event)
         else:
             log.debug("No events registered for event '%s'", str(event))
 
     def block_event(self, event):
         if event in self.event_handlers:
-            self.event_handlers[event][EVENT_BLOCKER_INDEX] += 1
+            self.event_handlers[event].blocker_tokens.append(True)
         else:
             log.debug("Trying to block an unknown event: %s", str(event))
 
     def unblock_event(self, event):
         if event in self.event_handlers:
-            if self.event_handlers[event][EVENT_BLOCKER_INDEX] > 0:
-                self.event_handlers[event][EVENT_BLOCKER_INDEX] -= 1
+            if self.event_handlers[event].blocker_tokens > 0:
+                self.event_handlers[event].blocker_tokens.pop()
             else:
                 log.debug("Trying to unblock non-blocked event '%s'", str(event))
         else:
@@ -76,16 +72,16 @@ class EventCore(pycam.Gui.Settings.Settings):
 
     def register_ui_section(self, section, add_action, clear_action):
         if section not in self.ui_sections:
-            self.ui_sections[section] = [None, None]
-            self.ui_sections[section][UI_WIDGET_INDEX] = []
-        self.ui_sections[section][UI_FUNC_INDEX] = (add_action, clear_action)
+            self.ui_sections[section] = UISection(None, None, [])
+        self.ui_sections[section] = UISection(add_action, clear_action,
+                                              self.ui_sections[section].widgets)
         self._rebuild_ui_section(section)
 
     def unregister_ui_section(self, section):
         if section in self.ui_sections:
             ui_section = self.ui_sections[section]
-            while ui_section[UI_WIDGET_INDEX]:
-                ui_section[UI_WIDGET_INDEX].pop()
+            while ui_section.widgets:
+                ui_section.widgets.pop()
             del self.ui_sections[section]
         else:
             log.debug("Trying to unregister a non-existent ui section: %s", str(section))
@@ -93,32 +89,22 @@ class EventCore(pycam.Gui.Settings.Settings):
     def _rebuild_ui_section(self, section):
         if section in self.ui_sections:
             ui_section = self.ui_sections[section]
-            if ui_section[UI_FUNC_INDEX]:
-                add_func, clear_func = ui_section[UI_FUNC_INDEX]
-                ui_section[UI_WIDGET_INDEX].sort(key=lambda x: x[WIDGET_WEIGHT_INDEX])
-                clear_func()
-                for item in ui_section[UI_WIDGET_INDEX]:
-                    if item[WIDGET_ARGS_INDEX]:
-                        args = item[WIDGET_ARGS_INDEX]
-                    else:
-                        args = {}
-                    add_func(item[WIDGET_OBJ_INDEX], item[WIDGET_NAME_INDEX], **args)
+            if ui_section.add_func or ui_section.clear_func:
+                ui_section.widgets.sort(key=lambda x: x.weight)
+                ui_section.clear_func()
+                for item in ui_section.widgets:
+                    ui_section.add_func(item.obj, item.name, **(item.args or {}))
         else:
             log.debug("Failed to rebuild unknown ui section: %s", str(section))
 
     def register_ui(self, section, name, widget, weight=0, args_dict=None):
         if section not in self.ui_sections:
-            self.ui_sections[section] = [None, None]
-            self.ui_sections[section][UI_WIDGET_INDEX] = []
-        assert WIDGET_NAME_INDEX == 0
-        assert WIDGET_OBJ_INDEX == 1
-        assert WIDGET_WEIGHT_INDEX == 2
-        assert WIDGET_ARGS_INDEX == 3
-        current_widgets = [item[1] for item in self.ui_sections[section][UI_WIDGET_INDEX]]
+            self.ui_sections[section] = UISection(None, None, [])
+        current_widgets = [item.obj for item in self.ui_sections[section].widgets]
         if (widget is not None) and (widget in current_widgets):
             log.debug("Tried to register widget twice: %s -> %s", section, name)
             return
-        self.ui_sections[section][UI_WIDGET_INDEX].append((name, widget, weight, args_dict))
+        self.ui_sections[section].widgets.append(UIWidget(name, widget, weight, args_dict))
         self._rebuild_ui_section(section)
 
     def unregister_ui(self, section, widget):
@@ -127,12 +113,12 @@ class EventCore(pycam.Gui.Settings.Settings):
                 section = None
             ui_section = self.ui_sections[section]
             removal_list = []
-            for index, item in enumerate(ui_section[UI_WIDGET_INDEX]):
-                if item[WIDGET_OBJ_INDEX] == widget:
+            for index, item in enumerate(ui_section.widgets):
+                if item.obj == widget:
                     removal_list.append(index)
             removal_list.reverse()
             for index in removal_list:
-                ui_section[UI_WIDGET_INDEX].pop(index)
+                ui_section.widgets.pop(index)
             self._rebuild_ui_section(section)
         else:
             log.debug("Trying to unregister unknown ui section: %s", section)
@@ -140,13 +126,13 @@ class EventCore(pycam.Gui.Settings.Settings):
     def register_chain(self, name, func, weight=100):
         if name not in self.chains:
             self.chains[name] = []
-        self.chains[name].append((func, weight))
-        self.chains[name].sort(key=lambda item: item[CHAIN_WEIGHT_INDEX])
+        self.chains[name].append(UIChain(func, weight))
+        self.chains[name].sort(key=lambda item: item.weight)
 
     def unregister_chain(self, name, func):
         if name in self.chains:
             for index, data in enumerate(self.chains[name]):
-                if data[CHAIN_FUNC_INDEX] == func:
+                if data.func == func:
                     self.chains[name].pop(index)
                     break
             else:
@@ -157,7 +143,7 @@ class EventCore(pycam.Gui.Settings.Settings):
     def call_chain(self, name, *args, **kwargs):
         if name in self.chains:
             for data in self.chains[name]:
-                data[CHAIN_FUNC_INDEX](*args, **kwargs)
+                data.func(*args, **kwargs)
         else:
             log.debug("Called an unknown chain: %s", name)
 
