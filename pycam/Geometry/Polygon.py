SebKuzminsky/pycam

commit 52b801967407dd09c5b1ce21073ec5e0285fd162

removed unused code (reported by 'vulture')

pycam/Geometry/Polygon.py
@@ -44,8 +44,6 @@ class PolygonInTree(IDGenerator):
     """ This class is a wrapper around Polygon objects that is used for sorting.
     """
 
-    next_id = 0
-
     def __init__(self, polygon):
         super(PolygonInTree, self).__init__()
         self.start = polygon.get_points()[0]
@@ -65,12 +63,6 @@ class PolygonInTree(IDGenerator):
     def insert_if_child(self, other):
         if self.polygon.is_polygon_inside(other.polygon):
             self.children.append(other)
-
-    def remove_child(self, other):
-        try:
-            self.children.remove(other)
-        except ValueError:
-            pass
 
     def get_cost(self, other):
         return pdist(other.start, self.end)
@@ -994,194 +986,6 @@ class Polygon(TransformableContainer):
             if not result:
                 log.debug("Skipping offset polygon: polygon is inside of another one")
             return result
-
-    def get_offset_polygons_old(self, offset):
-        def get_parallel_line(line, offset):
-            if offset == 0:
-                return Line(line.p1, line.p2)
-            else:
-                cross_offset = pmul(pnormalized(pcross(line.dir, self.plane.n)), offset)
-                # Prolong the line at the beginning and at the end - to allow
-                # overlaps. Use factor "2" to take care for star-like structure
-                # where a complete convex triangle would get cropped (two lines
-                # get lost instead of just one). Use the "abs" value to
-                # compensate negative offsets.
-                in_line = pmul(line.dir, 2 * abs(offset))
-                return Line(psub(padd(line.p1, cross_offset), in_line),
-                            padd(padd(line.p2, cross_offset), in_line))
-
-        def do_lines_intersection(l1, l2):
-            """ calculate the new intersection between two neighbouring lines
-            """
-            # TODO: use Line.get_intersection instead of the code below
-            if l1.p2 == l2.p1:
-                # intersection is already fine
-                return
-            if (l1.p1 is None) or (l2.p1 is None):
-                # one line was already marked as obsolete
-                return
-            x1, x2, x3, x4 = l2.p1, l2.p2, l1.p1, l1.p2
-            a = psub(x2, x1)
-            b = psub(x4, x3)
-            c = psub(x3, x1)
-            # see http://mathworld.wolfram.com/Line-LineIntersection.html (24)
-            try:
-                factor = pdot(pcross(c, b), pcross(a, b)) / pnormsq(pcross(a, b))
-            except ZeroDivisionError:
-                l2.p1 = None
-                return
-            if not (0 <= factor < 1):
-                # The intersection is always supposed to be within p1 and p2.
-                l2.p1 = None
-            else:
-                intersection = padd(x1, pmul(a, factor))
-                if Line(l1.p1, intersection).dir != l1.dir:
-                    # Remove lines that would change their direction due to the
-                    # new intersection. These are usually lines that become
-                    # obsolete due to a more favourable intersection of the two
-                    # neighbouring lines. This appears at small corners.
-                    l1.p1 = None
-                elif Line(intersection, l2.p2).dir != l2.dir:
-                    # see comment above
-                    l2.p1 = None
-                elif l1.p1 == intersection:
-                    # remove invalid lines (zero length)
-                    l1.p1 = None
-                elif l2.p2 == intersection:
-                    # remove invalid lines (zero length)
-                    l2.p1 = None
-                else:
-                    # shorten both lines according to the new intersection
-                    l1.p2 = intersection
-                    l2.p1 = intersection
-
-        def simplify_polygon_intersections(lines):
-            finished = False
-            new_group = lines[:]
-            while not finished:
-                if len(new_group) > 1:
-                    # Calculate new intersections for each pair of adjacent
-                    # lines.
-                    for index in range(len(new_group)):
-                        if (index == 0) and (not self.is_closed):
-                            # skip the first line if the group is not closed
-                            continue
-                        # this also works for index==0 (closed groups)
-                        l1 = new_group[index - 1]
-                        l2 = new_group[index]
-                        do_lines_intersection(l1, l2)
-                # Remove all lines that were marked as obsolete during
-                # intersection calculation.
-                clean_group = [line for line in new_group if line.p1 is not None]
-                finished = len(new_group) == len(clean_group)
-                if (len(clean_group) == 1) and self.is_closed:
-                    new_group = []
-                    finished = True
-                else:
-                    new_group = clean_group
-            # remove all non-adjacent intersecting lines (this splits the group)
-            if len(new_group) > 0:
-                group_starts = []
-                index1 = 0
-                while index1 < len(new_group):
-                    index2 = 0
-                    while index2 < len(new_group):
-                        index_distance = min(abs(index2 - index1),
-                                             abs(len(new_group) - (index2 - index1)))
-                        # skip neighbours
-                        if index_distance > 1:
-                            line1 = new_group[index1]
-                            line2 = new_group[index2]
-                            intersection, factor = line1.get_intersection(line2)
-                            if intersection and (intersection != line1.p1) \
-                                    and (intersection != line1.p2):
-                                del new_group[index1]
-                                new_group.insert(index1, Line(line1.p1, intersection))
-                                new_group.insert(index1 + 1, Line(intersection, line1.p2))
-                                # Shift all items in "group_starts" by one if
-                                # they reference a line whose index changed.
-                                for i in range(len(group_starts)):
-                                    if group_starts[i] > index1:
-                                        group_starts[i] += 1
-                                if index1 + 1 not in group_starts:
-                                    group_starts.append(index1 + 1)
-                                # don't update index2 -> maybe there are other hits
-                            elif intersection and (intersection == line1.p1):
-                                if index1 not in group_starts:
-                                    group_starts.append(index1)
-                                index2 += 1
-                            else:
-                                index2 += 1
-                        else:
-                            index2 += 1
-                    index1 += 1
-                # The lines intersect each other
-                # We need to split the group.
-                if len(group_starts) > 0:
-                    group_starts.sort()
-                    groups = []
-                    last_start = 0
-                    for group_start in group_starts:
-                        groups.append(new_group[last_start:group_start])
-                        last_start = group_start
-                    # Add the remaining lines to the first group or as a new
-                    # group.
-                    if groups[0][0].p1 == new_group[-1].p2:
-                        groups[0] = new_group[last_start:] + groups[0]
-                    else:
-                        groups.append(new_group[last_start:])
-                    # try to find open groups that can be combined
-                    combined_groups = []
-                    for index, current_group in enumerate(groups):
-                        # Check if the group is not closed: try to add it to
-                        # other non-closed groups.
-                        if current_group[0].p1 == current_group[-1].p2:
-                            # a closed group
-                            combined_groups.append(current_group)
-                        else:
-                            # the current group is open
-                            for other_group in groups[index + 1:]:
-                                if other_group[0].p1 != other_group[-1].p2:
-                                    # This group is also open - a candidate
-                                    # for merging?
-                                    if other_group[0].p1 == current_group[-1].p2:
-                                        current_group.reverse()
-                                        for line in current_group:
-                                            other_group.insert(0, line)
-                                        break
-                                    if other_group[-1].p2 == current_group[0].p1:
-                                        other_group.extend(current_group)
-                                        break
-                            else:
-                                # not suitable open group found
-                                combined_groups.append(current_group)
-                    return combined_groups
-                else:
-                    # just return one group without intersections
-                    return [new_group]
-            else:
-                return None
-
-        new_lines = []
-        for line in self.get_lines():
-            new_lines.append(get_parallel_line(line, offset))
-        cleaned_line_groups = simplify_polygon_intersections(new_lines)
-        if cleaned_line_groups is None:
-            return None
-        else:
-            # remove all groups with a toggled direction
-            self_is_outer = self.is_outer()
-            groups = []
-            for lines in cleaned_line_groups:
-                group = Polygon(self.plane)
-                for line in lines:
-                    group.append(line)
-                if group.is_outer() == self_is_outer:
-                    # We ignore groups that changed the direction. These
-                    # parts of the original group are flipped due to the
-                    # offset.
-                    groups.append(group)
-            return groups
 
     def get_cropped_polygons(self, minx, maxx, miny, maxy, minz, maxz):
         """ crop a line group according to a 3d bounding box

pycam/Geometry/Line.py
@@ -131,11 +131,6 @@ class Line(IDGenerator, TransformableContainer):
     def point_with_length_multiply(self, l):
         return padd(self.p1, pmul(self.dir, l*self.len))
 
-    def get_length_line(self, length):
-        """ return a line with the same direction and the specified length
-        """
-        return Line(self.p1, padd(self.p1, pmul(self.dir, length)))
-
     def closest_point(self, p):
         v = self.dir
         if v is None:
@@ -143,9 +138,6 @@ class Line(IDGenerator, TransformableContainer):
             return self.p1
         l = pdot(self.p1, v) - pdot(p, v)
         return psub(self.p1, pmul(v, l))
-
-    def dist_to_point_sq(self, p):
-        return pdist_sq(p, self.closest_point(p))
 
     def dist_to_point(self, p):
         return pdist(p, self.closest_point(p))

pycam/Geometry/Matrix.py
@@ -168,30 +168,3 @@ def multiply_vector_matrix(v, m):
     return (v[0] * m[0][0] + v[1] * m[0][1] + v[2] * m[0][2],
             v[0] * m[1][0] + v[1] * m[1][1] + v[2] * m[1][2],
             v[0] * m[2][0] + v[1] * m[2][1] + v[2] * m[2][2])
-
-
-def multiply_matrix_matrix(m1, m2):
-    def multi(row1, col2):
-        return m1[row1][0] * m2[0][col2] + m1[row1][1] * m2[1][col2] + m1[row1][2] * m2[2][col2]
-    return ((multi(0, 0), multi(0, 1), multi(0, 2)),
-            (multi(1, 0), multi(1, 1), multi(1, 2)),
-            (multi(2, 0), multi(2, 1), multi(2, 2)))
-
-
-def get_inverse_matrix(m):
-    _a = m[1][1] * m[2][2] - m[1][2] * m[2][1]
-    _b = m[0][2] * m[2][1] - m[0][1] * m[2][2]
-    _c = m[0][1] * m[1][2] - m[0][2] * m[1][1]
-    _d = m[1][2] * m[2][0] - m[1][0] * m[2][2]
-    _e = m[0][0] * m[2][2] - m[0][2] * m[2][0]
-    _f = m[0][2] * m[1][0] - m[0][0] * m[1][2]
-    _g = m[1][0] * m[2][1] - m[1][1] * m[2][0]
-    _h = m[0][1] * m[2][0] - m[0][0] * m[2][1]
-    _k = m[0][0] * m[1][1] - m[0][1] * m[1][0]
-    det = m[0][0] * _a + m[0][1] * _d + m[0][2] * _g
-    if det == 0:
-        return None
-    else:
-        return ((_a / det, _b / det, _c / det),
-                (_d / det, _e / det, _f / det),
-                (_g / det, _h / det, _k / det))

pycam/Geometry/Model.py
@@ -155,10 +155,6 @@ class BaseModel(IDGenerator, TransformableContainer):
         for item in items:
             self.append(item)
 
-    def maxsize(self):
-        return max(abs(self.maxx), abs(self.minx), abs(self.maxy), abs(self.miny), abs(self.maxz),
-                   abs(self.minz))
-
     def subdivide(self, depth):
         model = self.__class__()
         for item in next(self):
@@ -218,7 +214,6 @@ class Model(BaseModel):
         # enable/disable kdtree
         self._use_kdtree = use_kdtree
         self._t_kdtree = None
-        self.__flat_groups_cache = {}
         self.__uuid = None
 
     def __len__(self):
@@ -255,7 +250,6 @@ class Model(BaseModel):
         if self._use_kdtree:
             self._t_kdtree = TriangleKdtree(self.triangles())
         self.__uuid = str(uuid.uuid4())
-        self.__flat_groups_cache = {}
         # the kdtree is up-to-date again
         self._dirty = False
 
@@ -294,50 +288,6 @@ class Model(BaseModel):
                   [len(p.get_lines()) for p in contour.get_polygons()])
         return contour
 
-    def get_flat_areas(self, min_area=None):
-        """ Find plane areas (combinations of triangles) bigger than 'min_area'
-        and ignore vertical planes. The result is cached.
-        """
-        if min_area not in self.__flat_groups_cache:
-            def has_shared_edge(t1, t2):
-                count = 0
-                for p in (t1.p1, t1.p2, t1.p3):
-                    if p in (t2.p1, t2.p2, t2.p3):
-                        count += 1
-                return count >= 2
-
-            groups = []
-            for t in self.triangles():
-                # Find all groups with the same direction (see 'normal') that
-                # share at least one edge with the current triangle.
-                touch_groups = []
-                if t.normal[2] == 0:
-                    # ignore vertical triangles
-                    continue
-                for group_index, group in enumerate(groups):
-                    if t.normal == group[0].normal:
-                        for group_t in group:
-                            if has_shared_edge(t, group_t):
-                                touch_groups.append(group_index)
-                                break
-                if len(touch_groups) > 1:
-                    # combine multiple areas with this new triangle
-                    touch_groups.reverse()
-                    combined = [t]
-                    for touch_group_index in touch_groups:
-                        combined.extend(groups.pop(touch_group_index))
-                    groups.append(combined)
-                elif len(touch_groups) == 1:
-                    groups[touch_groups[0]].append(t)
-                else:
-                    groups.append([t])
-            # check the size of each area
-            if min_area is not None:
-                groups = [group for group in groups
-                          if sum([t.get_area() for t in group]) >= min_area]
-            self.__flat_groups_cache[min_area] = groups
-        return self.__flat_groups_cache[min_area]
-
 
 class ContourModel(BaseModel):
 
@@ -354,7 +304,6 @@ class ContourModel(BaseModel):
         # there is always just one plane
         self._plane_groups = [self._plane]
         self._item_groups.append(self._plane_groups)
-        self._cached_offset_models = {}
         self._export_function = pycam.Exporters.SVGExporter.SVGExporterContourModel
 
     def __len__(self):
@@ -368,11 +317,6 @@ class ContourModel(BaseModel):
         for polygon in self.get_polygons():
             result.append(polygon.copy())
         return result
-
-    def reset_cache(self):
-        super(ContourModel, self).reset_cache()
-        # reset the offset model cache
-        self._cached_offset_models = {}
 
     def _merge_polygon_if_possible(self, other_polygon, allow_reverse=False):
         """ Check if the given 'other_polygon' can be connected to another
@@ -524,9 +468,6 @@ class ContourModel(BaseModel):
             # parent class)
             pass
 
-    def get_num_of_lines(self):
-        return sum([len(group) for group in self._line_groups])
-
     def get_polygons(self, z=None, ignore_below=True):
         if z is None:
             return self._line_groups
@@ -635,34 +576,6 @@ class ContourModel(BaseModel):
         else:
             return None
 
-    def get_offset_model_simple(self, offset, callback=None):
-        """ calculate a contour model that surrounds the current model with
-        a given offset.
-        This is mainly useful for engravings that should not proceed _on_ the
-        lines but besides these.
-        @value offset: shifting distance; positive values enlarge the model
-        @type offset: float
-        @value callback: function to call after finishing a single line.
-            It should return True if the user interrupted the operation.
-        @type callback: callable
-        @returns: the new shifted model
-        @rtype: pycam.Geometry.Model.Model
-        """
-        # use a cached offset model if it exists
-        if offset in self._cached_offset_models:
-            return self._cached_offset_models[offset]
-        result = ContourModel(plane=self._plane)
-        for group in self._line_groups:
-            new_groups = group.get_offset_polygons(offset)
-            if new_groups is not None:
-                for new_group in new_groups:
-                    result.append(new_group)
-            if callback and callback():
-                return None
-        # cache the result
-        self._cached_offset_models[offset] = result
-        return result
-
     def get_offset_model(self, offset, callback=None):
         result = ContourModel(plane=self._plane)
         for group in self.get_polygons():
@@ -671,63 +584,6 @@ class ContourModel(BaseModel):
             if callback and callback():
                 return None
         return result
-
-    def get_copy(self):
-        result = ContourModel(plane=self._plane)
-        for group in self.get_polygons():
-            result.append(group)
-        return result
-
-    def check_for_collisions(self, callback=None, find_all_collisions=False):
-        """ check if lines in different line groups of this model collide
-
-        Returns a pycam.Geometry.Point.Point instance in case of an
-        intersection.
-        Returns None if the optional "callback" returns True (e.g. the user
-        interrupted the operation).
-        Otherwise it returns False if no intersections were found.
-        """
-        def check_bounds_of_groups(g1, g2):
-            if (g1.minx <= g2.minx <= g1.maxx) or (g1.minx <= g2.maxx <= g1.maxx) \
-                    or (g2.minx <= g1.minx <= g2.maxx) or (g2.minx <= g1.maxx <= g2.maxx):
-                # the x boundaries overlap
-                if (g1.miny <= g2.miny <= g1.maxy) or (g1.miny <= g2.maxy <= g1.maxy) \
-                        or (g2.miny <= g1.miny <= g2.maxy) or (g2.miny <= g1.maxy <= g2.maxy):
-                    # also the y boundaries overlap
-                    if (g1.minz <= g2.minz <= g1.maxz) or (g1.minz <= g2.maxz <= g1.maxz) \
-                            or (g2.minz <= g1.minz <= g2.maxz) or (g2.minz <= g1.maxz <= g2.maxz):
-                        # z overlaps as well
-                        return True
-            return False
-        # check each pair of line groups for intersections
-        intersections = []
-        for index1, group1 in enumerate(self._line_groups[:-1]):
-            for index2, group2 in enumerate(self._line_groups):
-                if index2 <= index1:
-                    # avoid double-checks
-                    continue
-                # check if both groups overlap - otherwise skip this pair
-                if check_bounds_of_groups(group1, group2):
-                    # check each pair of lines for intersections
-                    for line1 in group1.get_lines():
-                        for line2 in group2.get_lines():
-                            intersection, factor = line1.get_intersection(line2)
-                            if intersection:
-                                if find_all_collisions:
-                                    intersections.append((index1, index2))
-                                else:
-                                    # return just the place of intersection
-                                    return intersection
-            # update the progress visualization and quit if requested
-            if callback and callback():
-                if find_all_collisions:
-                    return intersections
-                else:
-                    return None
-        if find_all_collisions:
-            return intersections
-        else:
-            return False
 
     def extrude(self, stepping=None, func=None, callback=None):
         """ do a spherical extrusion of a 2D model.

pycam/Geometry/intersection.py
@@ -27,39 +27,6 @@ from pycam.Geometry.PointUtils import padd, pcross, pdiv, pdot, pmul, pnorm, pno
 from pycam.Utils.polynomials import poly4_roots
 
 
-def isNear(a, b):
-    return abs(a - b) < epsilon
-
-
-def isZero(a):
-    return isNear(a, 0)
-
-
-def intersect_lines(xl, zl, nxl, nzl, xm, zm, nxm, nzm):
-    X = None
-    Z = None
-    try:
-        if isZero(nzl) and isZero(nzm):
-            pass
-        elif isZero(nzl) or isZero(nxl):
-            X = xl
-            Z = zm + (xm - xl) * nxm / nzm
-            return (X, Z)
-        elif isZero(nzm) or isZero(nxm):
-            X = xm
-            Z = zl - (xm - xl) * nxl / nzl
-            return (X, Z)
-        else:
-            X = (zl - zm + (xm * nxm / nzm - xl * nxl / nzl)) \
-                    / (nxm / nzm - nxl / nzl)
-            if X and xl < X and X < xm:
-                Z = zl + (X - xl) * nxl / nzl
-                return (X, Z)
-    except ZeroDivisionError:
-        pass
-    return (None, None)
-
-
 def intersect_cylinder_point(center, axis, radius, radiussq, direction, point):
     # take a plane along direction and axis
     n = pnormalized(pcross(direction, axis))

pycam/Gui/OpenGLTools.py
@@ -31,15 +31,6 @@ from pycam.Geometry import sqrt
 from pycam.Geometry.PointUtils import pcross, pnorm, pnormalized, psub
 
 
-def keep_gl_mode(func):
-    def keep_gl_mode_wrapper(*args, **kwargs):
-        prev_mode = GL.glGetIntegerv(GL.GL_MATRIX_MODE)
-        result = func(*args, **kwargs)
-        GL.glMatrixMode(prev_mode)
-        return result
-    return keep_gl_mode_wrapper
-
-
 def keep_matrix(func):
     def keep_matrix_wrapper(*args, **kwargs):
         pushed_matrix_mode = GL.glGetIntegerv(GL.GL_MATRIX_MODE)

pycam/Gui/Visualization.py
@@ -49,7 +49,6 @@ xdist = 0
 ydist = -1.0
 zdist = -8.0
 
-texture_num = 2
 light = 1
 shade_model = GL.GL_FLAT
 polygon_mode = GL.GL_FILL

pycam/Plugins/Bounds.py
@@ -36,9 +36,6 @@ class Bounds(pycam.Plugins.ListPluginBase):
     CATEGORIES = ["Bounds"]
 
     # mapping of boundary types and GUI control elements
-    BOUNDARY_TYPES = {
-        pycam.Toolpath.Bounds.TYPE_RELATIVE_MARGIN: "TypeRelativeMargin",
-        pycam.Toolpath.Bounds.TYPE_CUSTOM: "TypeCustom"}
     CONTROL_BUTTONS = ("TypeRelativeMargin", "TypeCustom",
                        "ToolLimit", "RelativeUnit", "BoundaryLowX",
                        "BoundaryLowY", "BoundaryLowZ", "BoundaryHighX",

commit 8ca7de2326dd4b1db774ce6fd1f1597860165a49

fix lingering reference to "get_length_line"
Commit 52b8019 broke the "engrave" process with active radius
compensation.
See the merge request #78 from SebKuzminsky, who tracked down this
problem to its root cause.

Additionally this commit contains some improved comments and a more
readable flow of condititions.

(cherry picked from commit e5ac1baf1434f2913ca4847875003751f9f18f06)

pycam/Geometry/Polygon.py
@@ -290,12 +290,13 @@ class Polygon(TransformableContainer):
     def is_connectable(self, line_or_point):
         if self.is_closed:
             return False
-        elif not self._points:
-            # empty polygons can be connected with any line
+        if not self._points:
+            # empty polygons can be connected with any line or point
             return True
-        if hasattr(line_or_point, "get_length_line"):
-            # it is a line
+        if isinstance(line_or_point, Line):
             line = line_or_point
+            # Test if the line can be connected to the start or the end of the polygon.
+            # The direction of the line is respected.
             if line.p1 == self._points[-1]:
                 return True
             elif line.p2 == self._points[0]:
@@ -303,8 +304,8 @@ class Polygon(TransformableContainer):
             else:
                 return False
         else:
-            # it is a point
             point = line_or_point
+            # Test if the point equals the first or the last point of the polygon.
             return (point == self._points[-1]) or (point == self._points[0])
 
     def next(self):