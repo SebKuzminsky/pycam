7f87cb393ebe7a5a463f676920dedf9f65b15d50
Lars Kruse <devel@sumpfralle.de>
2018-10-16 05:14:52 +0200

    SVGDirectImporter: skip empty polygons
    (cherry picked from commit e935650bc370a685b05024bf63ba83249f27bb38)

--- pycam/Importers/SVGDirectImporter.py
+++ pycam/Importers/SVGDirectImporter.py
@@ -238,7 +238,8 @@ def _get_polygons_from_svg_path(path, z, accuracy=0.1, min_interpolation_steps=5
                         raise
             previous_path_point = new_point
         previous_segment_end = current_segment_end
-    return polygons
+    # filter out all empty polygons
+    return [polygon for polygon in polygons if polygon]
 
 
 def get_polygons_from_path_groups(path_groups, z_level_map=None):
