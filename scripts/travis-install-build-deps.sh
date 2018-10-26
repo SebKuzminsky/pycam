a4d9a1de4954153f7b208566a282af6b0dcb7988
Lars Kruse <devel@sumpfralle.de>
2018-10-26 03:52:04 +0200

    travis: add missing requirements for tests

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -5,7 +5,9 @@ sudo apt-get install -y --no-install-recommends \
     devscripts \
     equivs \
     gir1.2-gtk-3.0 \
+    python3-flake8 \
     python3-gi \
+    python3-setuptools \
     python3-yaml
 mk-build-deps -i -r -s sudo \
 	-t 'apt-get --yes --no-install-recommends --no-install-suggests'
