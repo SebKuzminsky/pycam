69224247dc85ac00076c62c1feb005f018beb159
Lars Kruse <devel@sumpfralle.de>
2018-12-19 03:22:52 +0100

    Replace OpenGL visualization for support model preview with X3D

--- pycam/Plugins/VisualizeSupportModelPreview.py
+++ pycam/Plugins/VisualizeSupportModelPreview.py
@@ -21,53 +21,30 @@ import pycam.Plugins
 import pycam.workspace.data_models
 
 
-class OpenGLViewSupportModelPreview(pycam.Plugins.PluginBase):
+class VisualizeSupportModelPreview(pycam.Plugins.PluginBase):
 
-    DEPENDS = ["OpenGLWindow", "OpenGLViewModel"]
-    CATEGORIES = ["Visualization", "OpenGL", "Support bridges"]
+    DEPENDS = {"Visualization", "VisualizeModel"}
+    CATEGORIES = {"Visualization", "Support bridges"}
 
     def setup(self):
-        self.core.register_event("visualize-items", self.draw_support_preview)
         self.core.get("register_display_item")("show_support_preview",
                                                "Show Support Model Preview", 30)
         self.core.get("register_color")("color_support_preview", "Support model", 30)
+        self.core.register_chain("generate_x3d", self.generate_x3d)
         self.core.emit_event("visual-item-updated")
         return True
 
     def teardown(self):
-        self.core.unregister_event("visualize-items", self.draw_support_preview)
+        self.core.unregister_chain("generate_x3d", self.generate_x3d)
         self.core.get("unregister_display_item")("show_support_preview")
         self.core.get("unregister_color")("color_support_preview")
         self.core.emit_event("visual-item-updated")
 
-    def draw_support_preview(self):
+    def generate_x3d(self, tree):
         if not self.core.get("show_support_preview"):
             return
-        models = []
+        color = self.core.get("color_support_preview")
         for model_object in (self.core.get("current_support_models") or []):
             model = model_object.get_model()
             if model:
-                models.append(model)
-        if not models:
-            return
-        GL = self._GL
-        # disable lighting
-        if self.core.get("view_light"):
-            GL.glDisable(GL.GL_LIGHTING)
-        # show a wireframe
-        if self.core.get("view_polygon"):
-            GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
-        # change the color
-        col = self.core.get("color_support_preview")
-        color = (col["red"], col["green"], col["blue"], col["alpha"])
-        GL.glColor4f(*color)
-        # we need to wait until the color change is active
-        GL.glFinish()
-        # draw the models
-        self.core.call_chain("draw_models", models)
-        # enable lighting again
-        if self.core.get("view_light"):
-            GL.glEnable(GL.GL_LIGHTING)
-        # enable polygon fill mode again
-        if self.core.get("view_polygon"):
-            GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL)
+                tree.add_data_source(model.to_x3d(color))
