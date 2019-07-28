SebKuzminsky/pycam

commit 63d55c39845d9fd6ec66195c02c2242355974ed5

remove unconditional dependency on GTK for all plugins

pycam/Plugins/OpenGLWindow.py
@@ -30,7 +30,6 @@ try:
 except (ImportError, RuntimeError):
     GL_ENABLED = False
 
-import gtk
 
 from pycam.Geometry import number, sqrt
 from pycam.Geometry.PointUtils import pcross, pmul, pnormalized
@@ -78,7 +77,7 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
                            "'python-gtkglext1' to enable it.")
             return False
         if self.gui:
-            self.context_menu = gtk.Menu()
+            self.context_menu = self._gtk.Menu()
             self.window = self.gui.get_object("OpenGLWindow")
             self.core.get("configure-drag-drop-func")(self.window)
             self.initialized = False
@@ -113,7 +112,7 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
 
             def add_widget_to_window(item, name):
                 if len(detail_box.get_children()) > 0:
-                    sep = gtk.HSeparator()
+                    sep = self._gtk.HSeparator()
                     detail_box.pack_start(sep)
                     sep.show()
                 detail_box.pack_start(item)
@@ -166,10 +165,10 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
             # key binding
             self._gtk_handlers.append((self.window, "key-press-event", self.key_handler))
             # OpenGL stuff
-            glconfig = gtk.gdkgl.Config(mode=(gtk.gdkgl.MODE_RGBA
-                                              | gtk.gdkgl.MODE_DEPTH
-                                              | gtk.gdkgl.MODE_DOUBLE))
-            self.area = gtk.gtkgl.DrawingArea(glconfig)
+            glconfig = self._gtk.gdkgl.Config(mode=(self._gtk.gdkgl.MODE_RGBA
+                                                    | self._gtk.gdkgl.MODE_DEPTH
+                                                    | self._gtk.gdkgl.MODE_DOUBLE))
+            self.area = self._gtk.gtkgl.DrawingArea(glconfig)
             # first run; might also be important when doing other fancy
             # gtk/gdk stuff
 #           self.area.connect_after('realize', self.paint)
@@ -178,11 +177,11 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
             # resize window
             self._gtk_handlers.append((self.area, 'configure-event', self._resize_window))
             # catch mouse events
-            self.area.set_events((gtk.gdk.MOUSE
-                                  | gtk.gdk.POINTER_MOTION_HINT_MASK
-                                  | gtk.gdk.BUTTON_PRESS_MASK
-                                  | gtk.gdk.BUTTON_RELEASE_MASK
-                                  | gtk.gdk.SCROLL_MASK))
+            self.area.set_events((self._gtk.gdk.MOUSE
+                                  | self._gtk.gdk.POINTER_MOTION_HINT_MASK
+                                  | self._gtk.gdk.BUTTON_PRESS_MASK
+                                  | self._gtk.gdk.BUTTON_RELEASE_MASK
+                                  | self._gtk.gdk.SCROLL_MASK))
             self._gtk_handlers.extend((
                 (self.area, "button-press-event", self.mouse_press_handler),
                 (self.area, "motion-notify-event", self.mouse_handler),
@@ -255,9 +254,9 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
         #  - a checkbox for the preferences window
         #  - a tool item for the drop-down list in the 3D window
         #  - a menu item for the context menu in the 3D window
-        action = gtk.ToggleAction(name, label, "Show/hide %s" % label, None)
+        action = self._gtk.ToggleAction(name, label, "Show/hide %s" % label, None)
         action.connect("toggled", lambda widget: self.core.emit_event("visual-item-updated"))
-        checkbox = gtk.CheckButton(label)
+        checkbox = self._gtk.CheckButton(label)
         action.connect_proxy(checkbox)
         tool_item = action.create_tool_item()
         menu_item = action.create_menu_item()
@@ -312,13 +311,13 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
 
         def set_color_wrapper(obj):
             def set_gtk_color_by_dict(color):
-                obj.set_color(gtk.gdk.Color(int(color["red"] * GTK_COLOR_MAX),
-                                            int(color["green"] * GTK_COLOR_MAX),
-                                            int(color["blue"] * GTK_COLOR_MAX)))
+                obj.set_color(self._gtk.gdk.Color(int(color["red"] * GTK_COLOR_MAX),
+                                                  int(color["green"] * GTK_COLOR_MAX),
+                                                  int(color["blue"] * GTK_COLOR_MAX)))
                 obj.set_alpha(int(color["alpha"] * GTK_COLOR_MAX))
             return set_gtk_color_by_dict
 
-        widget = gtk.ColorButton()
+        widget = self._gtk.ColorButton()
         widget.set_use_alpha(True)
         wrappers = (get_color_wrapper(widget), set_color_wrapper(widget))
         self._color_settings[name] = {"name": name, "label": label, "weight": weight,
@@ -344,11 +343,12 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
         items = self._color_settings.values()
         items.sort(key=lambda item: item["weight"])
         for index, item in enumerate(items):
-            label = gtk.Label("%s:" % item["label"])
+            label = self._gtk.Label("%s:" % item["label"])
             label.set_alignment(0.0, 0.5)
-            color_table.attach(label, 0, 1, index, index + 1, xoptions=gtk.FILL, yoptions=gtk.FILL)
-            color_table.attach(item["widget"], 1, 2, index, index + 1, xoptions=gtk.FILL,
-                               yoptions=gtk.FILL)
+            color_table.attach(label, 0, 1, index, index + 1, xoptions=self._gtk.FILL,
+                               yoptions=self._gtk.FILL)
+            color_table.attach(item["widget"], 1, 2, index, index + 1, xoptions=self._gtk.FILL,
+                               yoptions=self._gtk.FILL)
         color_table.show_all()
 
     def toggle_3d_view(self, widget=None, value=None):
@@ -389,10 +389,10 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
             return
         # define arrow keys and "vi"-like navigation keys
         move_keys_dict = {
-            gtk.keysyms.Left: (1, 0),
-            gtk.keysyms.Down: (0, -1),
-            gtk.keysyms.Up: (0, 1),
-            gtk.keysyms.Right: (-1, 0),
+            self._gtk.keysyms.Left: (1, 0),
+            self._gtk.keysyms.Down: (0, -1),
+            self._gtk.keysyms.Up: (0, 1),
+            self._gtk.keysyms.Right: (-1, 0),
             ord("h"): (1, 0),
             ord("j"): (0, -1),
             ord("k"): (0, 1),
@@ -434,7 +434,7 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
         elif keyval in move_keys_dict.keys():
             self._last_view = None
             move_x, move_y = move_keys_dict[keyval]
-            if get_state() & gtk.gdk.SHIFT_MASK:
+            if get_state() & self._gtk.gdk.SHIFT_MASK:
                 # shift key pressed -> rotation
                 base = 0
                 factor = 10
@@ -625,26 +625,26 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
         except AttributeError:
             # this should probably never happen
             return
-        control_pressed = modifier_state & gtk.gdk.CONTROL_MASK
-        shift_pressed = modifier_state & gtk.gdk.SHIFT_MASK
-        if ((event.direction == gtk.gdk.SCROLL_RIGHT)
-                or ((event.direction == gtk.gdk.SCROLL_UP) and shift_pressed)):
+        control_pressed = modifier_state & self._gtk.gdk.CONTROL_MASK
+        shift_pressed = modifier_state & self._gtk.gdk.SHIFT_MASK
+        if ((event.direction == self._gtk.gdk.SCROLL_RIGHT)
+                or ((event.direction == self._gtk.gdk.SCROLL_UP) and shift_pressed)):
             # horizontal move right
             self.camera.shift_view(x_dist=-1)
-        elif (event.direction == gtk.gdk.SCROLL_LEFT) \
-                or ((event.direction == gtk.gdk.SCROLL_DOWN) and shift_pressed):
+        elif (event.direction == self._gtk.gdk.SCROLL_LEFT) \
+                or ((event.direction == self._gtk.gdk.SCROLL_DOWN) and shift_pressed):
             # horizontal move left
             self.camera.shift_view(x_dist=1)
-        elif (event.direction == gtk.gdk.SCROLL_UP) and control_pressed:
+        elif (event.direction == self._gtk.gdk.SCROLL_UP) and control_pressed:
             # zoom in
             self.camera.zoom_in()
-        elif event.direction == gtk.gdk.SCROLL_UP:
+        elif event.direction == self._gtk.gdk.SCROLL_UP:
             # vertical move up
             self.camera.shift_view(y_dist=1)
-        elif (event.direction == gtk.gdk.SCROLL_DOWN) and control_pressed:
+        elif (event.direction == self._gtk.gdk.SCROLL_DOWN) and control_pressed:
             # zoom out
             self.camera.zoom_out()
-        elif event.direction == gtk.gdk.SCROLL_DOWN:
+        elif event.direction == self._gtk.gdk.SCROLL_DOWN:
             # vertical move down
             self.camera.shift_view(y_dist=-1)
         else:

pycam/Plugins/Clipboard.py
@@ -19,8 +19,6 @@ along with PyCAM.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import StringIO
-# imported later (on demand)
-# import gtk
 
 import pycam.Plugins
 from pycam.Utils.locations import get_all_program_locations
@@ -41,9 +39,9 @@ class Clipboard(pycam.Plugins.PluginBase):
     CATEGORIES = ["System"]
 
     def setup(self):
+        if not self._gtk:
+            return False
         if self.gui:
-            import gtk
-            self._gtk = gtk
             self._gtk_handlers = []
             self.clipboard = self._gtk.clipboard_get()
             self.core.set("clipboard-set", self._copy_text_to_clipboard)

pycam/Plugins/FilenameDialog.py
@@ -24,8 +24,7 @@ import pycam.Plugins
 import pycam.Utils
 
 
-def _get_filters_from_list(filter_list):
-    import gtk
+def _get_filters_from_list(gtk, filter_list):
     result = []
     for one_filter in filter_list:
         current_filter = gtk.FileFilter()
@@ -68,8 +67,8 @@ class FilenameDialog(pycam.Plugins.PluginBase):
     CATEGORIES = ["System"]
 
     def setup(self):
-        import gtk
-        self._gtk = gtk
+        if not self._gtk:
+            return False
         self.last_dirname = None
         self.core.set("get_filename_func", self.get_filename_dialog)
         return True
@@ -102,7 +101,7 @@ class FilenameDialog(pycam.Plugins.PluginBase):
             dialog.get_content_area().pack_start(extra_widget, expand=False)
         # add filter for files
         if type_filter:
-            for file_filter in _get_filters_from_list(type_filter):
+            for file_filter in _get_filters_from_list(self._gtk, type_filter):
                 dialog.add_filter(file_filter)
         # guess the export filename based on the model's filename
         valid_templates = []

pycam/Plugins/Fonts.py
@@ -38,9 +38,7 @@ class Fonts(pycam.Plugins.PluginBase):
         self._fonts_cache = pycam.Utils.FontCache.FontCache(get_font_dir(), core=self.core)
         self.core.set("fonts", self._fonts_cache)
         # "font dialog" window
-        if self.gui:
-            import gtk
-            self._gtk = gtk
+        if self.gui and self._gtk:
             self.font_dialog_window = self.gui.get_object("FontDialog")
             window = self.font_dialog_window
             hide_window = lambda *args: self.toggle_font_dialog_window(state=False)
@@ -84,7 +82,7 @@ class Fonts(pycam.Plugins.PluginBase):
 
     def teardown(self):
         del self.core["fonts"]
-        if self.gui:
+        if self.gui and self._gtk:
             font_toggle = self.gui.get_object("ShowFontDialog")
             self.core.unregister_ui("edit_menu", None)
             self.core.unregister_ui("edit_menu", font_toggle)

pycam/Plugins/GtkConsole.py
@@ -39,9 +39,9 @@ class GtkConsole(pycam.Plugins.PluginBase):
     def setup(self):
         self._history = []
         self._history_position = None
+        if not self._gtk:
+            return False
         if self.gui:
-            import gtk
-            self._gtk = gtk
             self._console = code.InteractiveConsole(locals=self.core.get_namespace(),
                                                     filename="PyCAM")
             # redirect sys.stdin/stdout - "exec" always writes there

pycam/Plugins/Log.py
@@ -34,9 +34,9 @@ class Log(pycam.Plugins.PluginBase):
     CATEGORIES = ["System"]
 
     def setup(self):
+        if not self._gtk:
+            return False
         if self.gui:
-            import gtk
-            self._gtk = gtk
             # menu item and shortcut
             log_action = self.gui.get_object("ToggleLogWindow")
             self._gtk_handlers = []

pycam/Plugins/MemoryAnalyzer.py
@@ -22,8 +22,6 @@ along with PyCAM.  If not, see <http://www.gnu.org/licenses/>.
 import csv
 import StringIO
 
-import gobject
-
 import pycam.Plugins
 import pycam.Utils.log
 
@@ -37,9 +35,9 @@ class MemoryAnalyzer(pycam.Plugins.PluginBase):
     CATEGORIES = ["System"]
 
     def setup(self):
+        if not self._gtk:
+            return False
         if self.gui:
-            import gtk
-            self._gtk = gtk
             # menu item and shortcut
             self.toggle_action = self.gui.get_object("ToggleMemoryAnalyzerAction")
             self._gtk_handlers = []
@@ -108,7 +106,7 @@ class MemoryAnalyzer(pycam.Plugins.PluginBase):
         self.gui.get_object("MemoryAnalyzerLoadingLabel").show()
         for objname in ("MemoryAnalyzerRefreshButton", "MemoryAnalyzerCopyButton"):
             self.gui.get_object(objname).set_sensitive(False)
-        gobject.idle_add(self._refresh_data_in_background)
+        self._gobject.idle_add(self._refresh_data_in_background)
 
     def _refresh_data_in_background(self):
         if not self._guppy:

pycam/Plugins/ModelSupportGrid.py
@@ -31,8 +31,7 @@ class ModelSupportGrid(pycam.Plugins.PluginBase):
     CATEGORIES = ["Model", "Support bridges"]
 
     def setup(self):
-        if self.gui:
-            import gtk
+        if self.gui and self._gtk:
             grid_box = self.gui.get_object("SupportModelGridBox")
             grid_box.unparent()
             self.core.register_ui("support_model_type_selector", "Grid", "grid", weight=-10)
@@ -87,7 +86,7 @@ class ModelSupportGrid(pycam.Plugins.PluginBase):
             self.grid_adjustment_value = self.gui.get_object("SupportGridPositionManualAdjustment")
             self.grid_adjustment_value_control = self.gui.get_object(
                 "SupportGridPositionManualShiftControl")
-            self.grid_adjustment_value_control.set_update_policy(gtk.UPDATE_DISCONTINUOUS)
+            self.grid_adjustment_value_control.set_update_policy(self._gtk.UPDATE_DISCONTINUOUS)
             self._gtk_handlers.extend((
                 (self.grid_adjustment_value_control, "move-slider",
                  self.update_support_grid_manual_adjust),
@@ -117,15 +116,15 @@ class ModelSupportGrid(pycam.Plugins.PluginBase):
                                get_set_grid_adjustment_value)
             grid_distance_square.set_active(True)
             self.core.set("support_grid_distance_x", 10.0)
-            self.core.register_chain("get_support_models", self._get_support_models)
             # handlers
             self._event_handlers = (("support-model-changed", self.update_support_controls), )
             self.register_gtk_handlers(self._gtk_handlers)
             self.register_event_handlers(self._event_handlers)
+        self.core.register_chain("get_support_models", self._get_support_models)
         return True
 
     def teardown(self):
-        if self.gui:
+        if self.gui and self._gtk:
             self.core.unregister_chain("get_support_models", self._get_support_models)
             self.core.unregister_ui("support_model_type_selector", "grid")
             self.core.unregister_ui("support_model_settings",

commit 13fb8525ffd177cc9d3428d943323217a3f74a22

Handle missing GTK OpenGL module gracefully
As long as gtk.gdkgl is available, access to gtk.gdk.BUTTON_MASK1 (and
others) worked, since "import gtk.gdkgl" had the side effect of loading
the gtk.gdk module.  But plugin initialization broke, if the gtk.gdkgl
module cannot be imported.

The problem was introduced in 63d55c39845d9fd6ec66195c02c2242355974ed5.

Closes: https://github.com/SebKuzminsky/pycam/issues/143

pycam/Plugins/OpenGLWindow.py
@@ -22,7 +22,7 @@ import math
 
 # careful import
 try:
-    import gtk.gtkgl
+    import gtk.gtkgl  # noqa: F401
     import OpenGL.GL as GL
     import OpenGL.GLU as GLU
     import OpenGL.GLUT as GLUT
@@ -56,12 +56,6 @@ VIEWS = {
              "up": (0.0, 0.0, 1.0), "znear": 0.01, "zfar": 10000.0, "fovy": 30.0},
 }
 
-# buttons for rotating, moving and zooming the model view window
-BUTTON_ROTATE = gtk.gdk.BUTTON1_MASK
-BUTTON_MOVE = gtk.gdk.BUTTON2_MASK
-BUTTON_ZOOM = gtk.gdk.BUTTON3_MASK
-BUTTON_RIGHT = 3
-
 # floating point color values are only available since gtk 2.16
 GTK_COLOR_MAX = 65535.0
 
@@ -77,6 +71,13 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
                            "'python-gtkglext1' to enable it.")
             return False
         if self.gui:
+            # buttons for rotating, moving and zooming the model view window
+            # These definitions cannot be defined globally, since we may not rely on gtk being
+            # available.
+            self.BUTTON_ROTATE = self._gtk.gdk.BUTTON1_MASK
+            self.BUTTON_MOVE = self._gtk.gdk.BUTTON2_MASK
+            self.BUTTON_ZOOM = self._gtk.gdk.BUTTON3_MASK
+            self.BUTTON_RIGHT = 3
             self.context_menu = self._gtk.Menu()
             self.window = self.gui.get_object("OpenGLWindow")
             drag_n_drop_func = self.core.get("configure-drag-drop-func")
@@ -608,7 +609,7 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
     @check_busy
     @gtkgl_functionwrapper
     def context_menu_handler(self, widget, event):
-        if (event.button == self.mouse["pressed_button"] == BUTTON_RIGHT) \
+        if (event.button == self.mouse["pressed_button"] == self.BUTTON_RIGHT) \
                 and self.context_menu \
                 and (event.get_time() - self.mouse["pressed_timestamp"] < 300) \
                 and (abs(event.x - self.mouse["pressed_pos"][0]) < 3) \
@@ -671,7 +672,9 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
     def mouse_handler(self, widget, event):
         x, y, state = event.x, event.y, event.state
         if self.mouse["button"] is None:
-            if (state & BUTTON_ZOOM) or (state & BUTTON_ROTATE) or (state & BUTTON_MOVE):
+            if ((state & self.BUTTON_ZOOM)
+                    or (state & self.BUTTON_ROTATE)
+                    or (state & self.BUTTON_MOVE)):
                 self.mouse["button"] = state
                 self.mouse["start_pos"] = [x, y]
         else:
@@ -679,7 +682,7 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
             # a decent visualization).
             if event.get_time() - self.mouse["event_timestamp"] < 40:
                 return
-            elif state & self.mouse["button"] & BUTTON_ZOOM:
+            elif state & self.mouse["button"] & self.BUTTON_ZOOM:
                 self._last_view = None
                 # the start button is still active: update the view
                 start_x, start_y = self.mouse["start_pos"]
@@ -695,12 +698,12 @@ class OpenGLWindow(pycam.Plugins.PluginBase):
                     scale = 100
                 self.camera.scale_distance(scale)
                 self._paint_ignore_busy()
-            elif (state & self.mouse["button"] & BUTTON_MOVE) \
-                    or (state & self.mouse["button"] & BUTTON_ROTATE):
+            elif (state & self.mouse["button"] & self.BUTTON_MOVE) \
+                    or (state & self.mouse["button"] & self.BUTTON_ROTATE):
                 self._last_view = None
                 start_x, start_y = self.mouse["start_pos"]
                 self.mouse["start_pos"] = [x, y]
-                if (state & BUTTON_MOVE):
+                if (state & self.BUTTON_MOVE):
                     # Determine the biggest dimension (x/y/z) for moving the
                     # screen's center in relation to this value.
                     obj_dim = []